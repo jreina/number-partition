f8d431e7889bc915d70e0579b72c9131d0606c66
Johnny Reina <johnny.r.reina@gmail.com>
2018-04-19 23:37:14 -0500

    This didn't even need to be recursive and it's faster now.

--- number-partition.js
+++ number-partition.js
@@ -1,22 +1,4 @@
 /**
- *
- * @param {number} leftover
- * @param {number} partitionRemainder
- * @param {Array.<number>} partitions
- * @returns {Array.<number>}
- */
-const partitionR = (leftover, partitionRemainder, partitions) => {
-  const delta = Math.ceil(Math.random() * (leftover - partitionRemainder));
-  return partitionRemainder === 1
-    ? partitions.concat(leftover)
-    : partitionR(
-        leftover - delta,
-        partitionRemainder - 1,
-        partitions.concat(delta)
-      );
-};
-
-/**
  * Partition a given natural number, `x`, into `n` partitions. Quotients are accepted for `x` and `n` but are floored to integer values.
  * @param {number} n
  * @returns {(x: number) => Array.<number>}
@@ -33,7 +15,21 @@ const partitionInto = n => x => {
   if (x < 0) {
     throw new Error('Invalid arguments. Number to partition must be > 0.');
   }
-  return partitionR(Math.floor(x), Math.floor(n), []);
+  if (n === 1) {
+    return [x];
+  }
+  const p = Math.floor(n);
+  const q = Math.floor(x);
+  const partitionPositions = Array(p - 1)
+    .fill(0)
+    .map(() => Math.round(Math.random() * q))
+    .sort((x, y) => x - y);
+
+  const deltas = partitionPositions.map(
+    (v, i, a) => (i === 0 ? v : v - a[i - 1])
+  );
+
+  return deltas.concat(q - partitionPositions[p - 2]);
 };
 
 module.exports = partitionInto;
